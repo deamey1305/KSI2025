2db638aca52543563745f899fd8b25e4be9e4ec4
deamey1305 <nadheaameylia@gmail.com>
2025-12-01 14:25:16 +0700

    test webhook

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-<?php echo 'Program Utama'; ?>
+<?php
+echo "Program Utama";
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+</head>
+<body>
+
+    <h1>Selamat Datang di Program Utama</h1>
+    <p>Ini adalah halaman index Anda.</p>
+
+</body>
+</html>
+
